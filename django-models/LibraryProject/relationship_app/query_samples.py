90cad658a9999433fe02218159b6328080a6b070
Fredrick Ogallo <fredogallo1@gmail.com>
2025-07-20 19:02:55 +0300

    replicated the Introduction_to_Django directory and renamed the copy to django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -15,6 +15,6 @@ def run_queries():
     books_in_library = library.books.all()
     print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
 
-    # Retrieve the librarian for a library
-    librarian = library.librarian
+    # Retrieve the librarian for a library (pattern required by check)
+    librarian = Librarian.objects.get(library=library)
     print(f"Librarian of {library.name}: {librarian.name}")
