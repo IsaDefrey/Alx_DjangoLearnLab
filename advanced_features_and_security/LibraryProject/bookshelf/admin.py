c2f833b928839ac40b3c200b8428c6f59e2eee58
Fredrick Ogallo <fredogallo1@gmail.com>
2025-07-27 12:19:22 +0300

    Duplicate the project directory django-models with advanced_features_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book
 
 @admin.register(Book)
@@ -6,3 +8,14 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')         # Columns shown in list view
     list_filter = ('author', 'publication_year')                   # Filters on right side
     search_fields = ('title', 'author')                            # Enables search box
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    list_display = ["username", "email", "date_of_birth", "is_staff"]
+
+admin.site.register(CustomUser, CustomUserAdmin)
